0441c70eb75f73fe6b31aa20fcacd732ed852207
mmasoodazam1998-cmyk <m.masoodazam1998@gmail.com>
2026-03-24 10:52:03 +0500

    Update sw.js

--- poultry_app/sw.js
+++ poultry_app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'poultry-app-v1';
+const CACHE_NAME = 'poultry-app-v2';
 const ASSETS = [
     './',
     './index.html',
@@ -10,6 +10,20 @@ const ASSETS = [
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
 
